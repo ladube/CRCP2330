8bf90dcb5a769f74db3251b6263652df59e03a15
ladube <ldube@smu.edu>
2020-03-02 10:30:44 -0600

    Mult.asm Pseudocode

--- nand2tetris/nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,13 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//(LOOP) to add R0 R1 times
+	//R2 = R2 + R0
+	//R1 --
+	//JEQ R1  <-- if R1 = 0, Jump to End
+	//otherwise @LOOP
+//(END) 
+	@End
+	0;JMP
+	

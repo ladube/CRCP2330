a6746b7eeb38646768c11f6c8ec7a3968fea9d6b
ladube <ldube@smu.edu>
2020-03-04 10:17:55 -0600

    Implemented Loop in Mult.asm

--- nand2tetris/nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/nand2tetris/projects/04/mult/mult.asm
@@ -9,11 +9,14 @@
 // Put your code here.
 
 //(LOOP) to add R0 R1 times
-	//R2 = R2 + R0
-	//R1 --
-	//JEQ R1  <-- if R1 = 0, Jump to End
-	//otherwise @LOOP
-//(END) 
+(LOOP)
+	@END
+	R1;JEQ //if R1 = 0 , Jump to End
+		//R2 = R2 + R0
+		//R1 --
+	@LOOP
+	0;JMP //loops back
+(END) 
 	@End
-	0;JMP
+	0;JMP //infinite loop
 	
